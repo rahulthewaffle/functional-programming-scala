b3e05ec5743bf0416e94e46320d78f33d7f13827
Rahul Devanarayanan <rahul.sairam@gmail.com>
2021-03-15 17:59:26 -0700

    Implemented keyword filtering for tweetsets

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -211,8 +211,10 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  def keywordTweetFilter(keywordList: List[String]): TweetSet = TweetReader.allTweets.filter(tweet=>keywordList.exists(keyword=>tweet.text.contains(keyword)))
+
+  lazy val googleTweets: TweetSet = keywordTweetFilter(google)
+  lazy val appleTweets: TweetSet = keywordTweetFilter(apple)
 
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
