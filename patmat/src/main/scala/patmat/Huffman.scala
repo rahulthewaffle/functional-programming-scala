2aef531dee52b4cbffe4d05d32ec1c09d228f8f7
Rahul Devanarayanan <rahul.sairam@gmail.com>
2021-03-19 17:21:44 -0700

    Impemented sorting and conversion of freqs of List

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -107,7 +107,10 @@ trait Huffman extends HuffmanInterface {
    * head of the list should have the smallest weight), where the weight
    * of a leaf is the frequency of the character.
    */
-  def makeOrderedLeafList(freqs: List[(Char, Int)]): List[Leaf] = ???
+  def makeOrderedLeafList(freqs: List[(Char, Int)]): List[Leaf] = {
+    freqs.sortWith((a,b) => a._2 < b._2).map(a => Leaf(a._1, a._2))
+//    orderedFreqs.map(a => Leaf(a._1, a._2))
+  }
 
   /**
    * Checks whether the list `trees` contains only one single code tree.
